1681ad9e9a98675cf6c22e380c5536fe4ea32002
Jason Edelman <edelmanja@gmail.com>
2025-07-15 20:36:08 -0400

    use nested functions for dbs

--- db.py
+++ db.py
@@ -24,19 +24,12 @@ def get_db(db_code):
     return all
 
 @st.cache_data
-def get_work_order_db():
-    return get_db(WORK_ORDERS)
-    
-@st.cache_data
-def get_complaint_db():
-    return get_db(COMPLAINTS)
-
-@st.cache_data
-def get_expenditure_db():
-    return get_db(EXPENDITURES)
-
-@st.cache_data
 def get_work_orders_from_local_db(query=''):
+    @st.cache_data
+    def get_work_order_db():
+        return get_db(WORK_ORDERS)
+
+
     alldata = get_work_order_db()
     if(query == ''):
         return alldata
@@ -45,6 +38,11 @@ def get_work_orders_from_local_db(query=''):
 
 @st.cache_data
 def get_complaints_from_local_db(query=''):
+    @st.cache_data
+    def get_complaint_db():
+        return get_db(COMPLAINTS)
+
+
     alldata = get_complaint_db()
     if(query == ''):
         return alldata
@@ -53,6 +51,10 @@ def get_complaints_from_local_db(query=''):
 
 @st.cache_data
 def get_expenditures_from_local_db(query=''):
+    @st.cache_data
+    def get_expenditure_db():
+        return get_db(EXPENDITURES)
+
     alldata = get_expenditure_db()
     if(query == ''):
         return alldata
